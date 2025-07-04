320664aacbccf035982e5e53ec44afcb61d92a42
ideftbuild <a.akanSWE@gmail.com>
2025-07-04 23:11:11 +0100

    update(UI): change clouds positioning

--- src/app/background/components/Clouds.tsx
+++ src/app/background/components/Clouds.tsx
@@ -2,11 +2,10 @@ import Cloud from './Cloud';
 
 const Clouds = () =>  {
   const cloudData = [
-    { delay: 0, xPosition: 10, yPosition: 5, scale: 1 },
-    { delay: 3, xPosition: 30, yPosition: 10, scale: 1.2 },
-    { delay: 6, xPosition: 60, yPosition: 7, scale: 0.9 },
-    { delay: 9, xPosition: 80, yPosition: 15, scale: 1.1 },
-    { delay: 12, xPosition: 100, yPosition: 20, scale: 1.5 },
+    { delay: 3, xPosition: 40, yPosition: 10, scale: 1.2 },
+    { delay: 3.2, xPosition: 47, yPosition: 7, scale: 0.9 },
+    { delay: 3.4, xPosition: 57, yPosition: 15, scale: 1.1 },
+    { delay: 3.6, xPosition: 67, yPosition: 20, scale: 1.5 },
   ];
 
   return (
